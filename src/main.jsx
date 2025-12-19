0876d12d10c5c8af42099206f4444ad61399f3b5
Toniinnb <toniinnb@gmail.com>
2025-12-20 12:25:19 +1300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { ConfigProvider } from './ConfigContext' // 引入配置引擎
+import { ConfigProvider } from './ConfigContext' // 👈 必须有这一行
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* 👇 必须用这个包住 App，否则所有页面都会白屏 */}
     <ConfigProvider>
       <App />
     </ConfigProvider>

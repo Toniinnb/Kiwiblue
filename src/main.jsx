4ccf4cb0a473ed8208a814a90dea38cfaa930cb3
Toniinnb <toniinnb@gmail.com>
2025-12-20 12:08:06 +1300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { ConfigProvider } from './ConfigContext' // 引入配置引擎
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider>
+      <App />
+    </ConfigProvider>
   </React.StrictMode>,
 )

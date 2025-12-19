d7c59e03766ecd1204ab47cf6edb87680276b2bd
Toniinnb <toniinnb@gmail.com>
2025-12-19 17:12:38 +1300

    Update Supabase URL and anon key
    Updated Supabase URL and key to new values.

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // ⚠️ 请把下面两行引号里的内容，换成您刚才复制的真实 URL 和 Key
-const supabaseUrl = 'https://您的ProjectURL.supabase.co'
-const supabaseKey = '您的anon_key_一大长串'
+const supabaseUrl = 'https://ptjficullimxdxrqhgqd.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0amZpY3VsbGlteGR4cnFoZ3FkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYxMDcyNDgsImV4cCI6MjA4MTY4MzI0OH0.atmDyxySFRvIliZqpcsORQWK63REKUEjLB4_vSgyPYM'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
